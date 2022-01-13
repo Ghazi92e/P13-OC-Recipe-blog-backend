8b14660ffde6347375914aa047c570d5b16c7a24
Ghazi <ghaziatie@gmail.com>
2022-01-13 11:28:46 +0100

    configure view uploadfile

---
+++ uploadfile/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+from rest_framework import viewsets
+from uploadfile.models import Uploadfile
+from uploadfile.serializers import UploadfileSerializer
+
+class UploadfileViewSet(viewsets.ModelViewSet):
+
+    queryset = Uploadfile.objects.all()
+    serializer_class = UploadfileSerializer
+    filterset_fields = ['id']
