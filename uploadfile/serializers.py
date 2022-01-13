75cd92ce7d518a82a6d19f00fcfc581496dc8142
Ghazi <ghaziatie@gmail.com>
2022-01-13 11:27:16 +0100

    configure serializers

---
+++ uploadfile/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+
+from uploadfile.models import Uploadfile
+
+class UploadfileSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Uploadfile
+        fields = ['id', 'file']
