f9b9e3c2fafc2bb4ed1e7a5c61066f9cc9e11584
Ghazi <ghaziatie@gmail.com>
2022-01-13 11:28:16 +0100

    configure urls app uploadfile

---
+++ uploadfile/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from rest_framework.routers import DefaultRouter
+from uploadfile import views
+from django.urls import include
+
+router = DefaultRouter()
+router.register(r'api-upload-file', views.UploadfileViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
