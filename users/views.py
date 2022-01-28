3db132c75cbc50f971443b1efb226d4ef5e08d90
Ghazi <ghaziatie@gmail.com>
2022-01-29 00:57:09 +0100

    create action to get user favorite recipes

--- users/views.py
+++ users/views.py
@@ -3,15 +3,19 @@ from rest_framework.response import Response
 from django.contrib.auth.models import User
 from django.contrib.auth import get_user_model
 from rest_framework import viewsets
+from favoriterecipe.serializers import FavoriteRecipeSerializer
+from favoriterecipe.models import FavoriteRecipe
 from uploadfile.models import Uploadfile
-from users.serializers import UsersSerializer
+from users.serializers import UsersFavoriteRecipesSerializer, UsersSerializer
+from rest_framework.decorators import action
+
 
 User = get_user_model()
 class UserViewSet(viewsets.ModelViewSet):
     
     queryset = User.objects.all()
     serializer_class = UsersSerializer
-    filterset_fields = ['auth_token', 'id']
+    filterset_fields = ['auth_token', 'id', 'favorite_recipes']
 
 
     def perform_create(self, serializer):
@@ -26,6 +30,50 @@ class UserViewSet(viewsets.ModelViewSet):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+
+    @action(detail=True, methods=['get', 'post', 'delete'])
+    def favorite_recipes(self, request, pk):
+        queryset = User.objects.get(pk=pk)
+        if request.method == 'GET':
+            serializer = UsersFavoriteRecipesSerializer(queryset)
+            return Response(serializer.data)
+
+        elif request.method == 'POST':
+            serializer = FavoriteRecipeSerializer(data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+        elif request.method == 'DELETE':
+            querysetdata = FavoriteRecipe.objects.get(user=pk, recipe=request.data.get('recipe'))
+            querysetdata.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        
+
+
+    # @action(detail=True, methods=['post'])
+    # def favorite_recipes(self, request, pk=None):
+    #     datatest = User.objects.all().filter(pk=pk)
+    #     serializer = UsersFavoriteRecipesSerializer(datatest, data=request.data)
+    #     if serializer.is_valid():
+    #         serializer.save()
+    #         return Response(serializer.data)
+    #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    # @action(detail=True, methods=['put'])
+    # def delete_favorite_recipes(self, request, pk=None):
+    #     user_recipe = FavoriteRecipe.objects.all().filter(user=pk).first()
+    #     serializer = FavoriteRecipeSerializer(user_recipe, data=request.data)
+    #     if serializer.is_valid():
+    #         serializer.save()
+    #         return Response(serializer.data)
+    #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+        # favorite_recipes = user_recipe.favorite_recipes.all()
+        # remove = request.data['favorite_recipes']
+        # datatest.delete()
 
 
 
