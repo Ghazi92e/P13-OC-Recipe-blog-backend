108ab004a1f8100400c89cabf4de885f3c2b5091
Ghazi <ghaziatie@gmail.com>
2022-02-20 22:59:40 +0100

    create serializers UserFavoriteRecipes

--- favoriterecipe/serializers.py
+++ favoriterecipe/serializers.py
@@ -1,7 +1,15 @@
 from rest_framework import serializers
+from recipes.serializers import RecipesSerializer
 from favoriterecipe.models import FavoriteRecipe
 
 class FavoriteRecipeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = FavoriteRecipe
+        fields = ['id', 'user', 'recipe']
+
+
+class UserFavoriteRecipesSerializer(serializers.ModelSerializer):
+    recipe = RecipesSerializer()
     class Meta:
         model = FavoriteRecipe
         fields = ['id', 'user', 'recipe']
