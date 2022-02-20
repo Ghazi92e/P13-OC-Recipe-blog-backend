95bbc9156912c864032c57fb27e69ac0662296a7
Ghazi <ghaziatie@gmail.com>
2022-02-20 23:08:39 +0100

    add field image_url to model User

--- users/models.py
+++ users/models.py
@@ -1,16 +1,15 @@
 from django.contrib.auth.base_user import BaseUserManager
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-# from favorite_recipe.models import FavoriteRecipe
 
 from uploadfile.models import Uploadfile
 
 # Create your models here.
 class MyUserManager(BaseUserManager):
-    def create_user(self, username, email, password, file):
+    def create_user(self, username, email, password, file, image_url):
         """
-        Creates and saves a User with the given email, date of
-        birth and password.
+        Creates and saves a User with the given email, file
+        and password.
         """
         if not email:
             raise ValueError('Users must have an email address')
@@ -19,7 +18,8 @@ class MyUserManager(BaseUserManager):
             username=username,
             email=self.normalize_email(email),
             password=password,
-            file=file
+            file=file,
+            image_url=image_url
         )
 
         user.set_password(password)
@@ -30,5 +30,7 @@ class CustomUser(AbstractUser):
         unique=True,
     )
     file = models.ForeignKey(Uploadfile, on_delete=models.CASCADE)
+    image_url = models.URLField()
     objects = MyUserManager()
-    favorite_recipes = models.ManyToManyField('recipes.Recipes', through='favoriterecipe.FavoriteRecipe')
+    # favorite_recipes = models.ManyToManyField('recipes.Recipes', through='favoriterecipe.FavoriteRecipe')
+    # user_following = models.ManyToManyField('relationships.Relationships')
