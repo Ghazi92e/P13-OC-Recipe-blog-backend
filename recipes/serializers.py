c16f69529fa6d73b435eddafcc25e713926f2ba4
Ghazi <ghaziatie@gmail.com>
2022-02-20 23:02:04 +0100

    create RecipesUsernameImageSerializer

--- recipes/serializers.py
+++ recipes/serializers.py
@@ -1,9 +1,18 @@
 from django.db.models import fields
 from rest_framework import serializers
+from structure.serializers import UsersSerializer
 from recipes.models import Recipes
 
 
 class RecipesSerializer(serializers.ModelSerializer):
     class Meta:
         model = Recipes
-        fields = ['id', 'title', 'description', 'category', 'file', 'user']
+        fields = ['id', 'title', 'description', 'category', 'file', 'user', 'image_url']
+
+
+
+class RecipesUsernameImageSerializer(serializers.ModelSerializer):
+    user = UsersSerializer()
+    class Meta:
+        model = Recipes
+        fields = ['id', 'title', 'description', 'category', 'file', 'user', 'image_url']
