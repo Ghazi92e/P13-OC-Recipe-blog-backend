6031e5108096d15b9edcf98cda6cf75b6a6efebe
Ghazi <ghaziatie@gmail.com>
2022-03-21 00:16:29 +0100

    add field updated and ingredients for recipes model

--- recipes/models.py
+++ recipes/models.py
@@ -8,7 +8,9 @@ User = get_user_model()
 
 class Recipes(models.Model):
     created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
     title = models.CharField(max_length=100, blank=True)
+    ingredients = models.TextField(blank=True)
     description = models.TextField()
     file = models.ForeignKey(Uploadfile, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_recipes')
