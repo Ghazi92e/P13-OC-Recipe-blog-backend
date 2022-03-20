95878eeb3807181f2cd995eb2dcdd59bfc6b59a5
Ghazi <ghaziatie@gmail.com>
2022-03-21 00:20:32 +0100

    set permissions for recipe app

--- recipes/views.py
+++ recipes/views.py
@@ -1,11 +1,27 @@
 from rest_framework import viewsets
 from recipes.models import Recipes
 from recipes.serializers import RecipesSerializer
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import permissions
 
 
+class IsUserOrReadOnly(permissions.BasePermission):
+    """
+    Object-level permission to only allow owners of an object to edit it.
+    Assumes the model instance has an `owner` attribute.
+    """
+
+    def has_object_permission(self, request, view, obj):
+        # Read permissions are allowed to any request,
+        # so we'll always allow GET, HEAD or OPTIONS requests.
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        else:
+            # Instance must have an attribute named `owner`.
+            return obj.user == request.user
 
 class RecipeViewSet(viewsets.ModelViewSet):
-
+    permission_classes = [permissions.IsAuthenticated, IsUserOrReadOnly]
     queryset = Recipes.objects.all()
     serializer_class = RecipesSerializer
     filterset_fields = ['category', 'category__name', 'user', 'user__username']
