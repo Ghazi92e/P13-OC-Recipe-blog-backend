de21734d8c054f2c8b903ad32e2b245dbae9b278
Ghazi <ghaziatie@gmail.com>
2022-03-25 16:24:50 +0100

    add host database 'localhost'

--- blog_lebanese_recipes/settings.py
+++ blog_lebanese_recipes/settings.py
@@ -66,12 +66,6 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': [
         'django_filters.rest_framework.DjangoFilterBackend'
     ],
-    'TEST_REQUEST_RENDERER_CLASSES': [
-        'rest_framework.renderers.MultiPartRenderer',
-        'rest_framework.renderers.JSONRenderer',
-        'rest_framework.renderers.TemplateHTMLRenderer'
-    ],
-    'TEST_REQUEST_DEFAULT_FORMAT': 'json',
     # 'DEFAULT_PERMISSION_CLASSES': [
     #     'rest_framework.permissions.IsAuthenticated',
     # ]
@@ -123,7 +117,7 @@ DATABASES = {
         'NAME': env('DATABASE_NAME'),
         'USER': env('DATABASE_USER'),
         'PASSWORD': env('DATABASE_PASS'),
-        'HOST': '',
+        'HOST': 'localhost',
         'PORT': '5432',
     }
 }
