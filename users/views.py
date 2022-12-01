240e1bd17879d7790dc6083c503c8cb8654a082d
Ghazi <ghaziatie@gmail.com>
2022-12-01 10:57:51 +0100

    let superuser edit users account

--- users/views.py
+++ users/views.py
@@ -26,6 +26,8 @@ class IsAdminUserOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         # Read permissions are allowed to any request,
         # so we'll always allow GET, HEAD or OPTIONS requests.
+        if (request.user.is_superuser is True):
+            return True
         if request.method in permissions.SAFE_METHODS:
             return True
         if obj.id == request.user.id:
